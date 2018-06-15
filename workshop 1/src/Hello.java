55ec601575d9be0d97e870538d6b67ea53eff769
q7796952irene <jiaqiz4@student.unimelb.edu.au>
2018-06-16 01:02:19 +1000

    1st

---
+++ workshop 1/src/Hello.java
@@ -0,0 +1,15 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		
+        System.out.println("Hello, World");
+        while (i<5){
+        	System.out.println(args[i]);
+        	i++;
+        }
+	}
+
+}
